a002b813c9659fcb42c82b8e7532c86a7cbfd80f
Athora190 <jademarte7@gmail.com>
2018-07-31 09:46:47 -0400

    app

--- week1/labs-js/app.js
+++ week1/labs-js/app.js
@@ -1,3 +1,17 @@
 function sendAlert() {
-  alert("Hello Everyone from CSSI ");
+console.log("Hello Everyone from CSSI ");
 }
+var name1='Bob';
+let name2='Bill';
+const name3='Bo';
+console.log(name1);
+console.log(name2);
+console.log(name3);
+//resaigning names
+
+name1='Amy';
+name2='Hanna';
+//name3='April';
+console.log(name1);
+console.log(name2);
+console.log(name3);
