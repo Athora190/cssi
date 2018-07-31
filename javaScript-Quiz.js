582a0750fd168c662374d35f530aa6b336442da9
Athora190 <jademarte7@gmail.com>
2018-07-31 09:44:26 -0400

    Quiz

---
+++ javaScript-Quiz.js
@@ -0,0 +1,54 @@
+//1.
+function functionName(); {
+
+}
+console.log("Hello World");
+
+//2.
+let fname="first name";
+let lname=" last name";
+console.log(lname,fname);
+
+//3.
+let x=13;
+let y=14;
+let x=14;
+let y=13;
+
+//4.
+if (isOn && !lightUp || !isOn && lightsUp) {
+  console.log("Fualty switch");
+}
+else {
+  console.log("Working correctly");
+}
+
+//5.
+if (x>y) {
+  console.log("y is greater then x");
+}
+else {
+  console.log("x is greater then y ");
+}
+
+//6.
+if (num<=-50) {
+  console.log("true");
+}
+else {
+  console.log("false");
+}
+
+//7.
+32
+
+//8.
+for (var i = 1; i <= 100; i++) {
+  console.log(i);
+ }
+
+ //9.
+ for (var i = 1; i <= 100;i++) {
+   if (i==17 && 43) {
+     
+   }
