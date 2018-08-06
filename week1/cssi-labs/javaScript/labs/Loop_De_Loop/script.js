454aa3ec9c3326add85afb0cfa451f4ed1198ddc
Athora190 <jademarte7@gmail.com>
2018-08-06 12:42:09 -0400

    looplab

--- week1/cssi-labs/javaScript/labs/Loop_De_Loop/script.js
+++ week1/cssi-labs/javaScript/labs/Loop_De_Loop/script.js
@@ -11,17 +11,35 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+console.log('Script is running');
 let amusementParks = ["Disney World", "Hershey Park", "Six Flags", "Dorney Park", "Sea World"];
 //Complete task one below
 
+function Amusementparks(value,index,array){
+   value = value.toUpperCase();
 
+console.log(value +' is a great amusment park.');
+}
+amusementParks.forEach(Amusementparks);
 
 
 let rollerCoasters = ["Big Thunder Mountain Railroad","Splash Mountain","Space Mountain", "Expedition Everest","The Twilight Zone Tower of Terror"];
 //Complete task two below
-
+for (var i = 0; i < rollerCoasters.length; i++) {
+  let ride = rollerCoasters[i]
+  console.log('I will ride ' + ride + " for ever roller coaster up to Space Mountain.")
+  if(ride === "Space Mountain"){
+     break;
+   }
+}
 
 
 let height = 6;
 //Complete task three below
+let number = Math.floor(Math.random() * 6) + 2;
+while (number > 4) {
+
+  console.log('Next Customer', number);
+  number = Math.floor(Math.random() * 6) + 2;
+
+}
