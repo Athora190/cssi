2e6beb4eb29c8ae33d8d727cc0a87b6ce90e6503
Athora190 <jademarte7@gmail.com>
2018-08-07 12:03:27 -0400

    lab

---
+++ week1/cssi-labs/python/labs/make-a-short-story/mystory.py
@@ -0,0 +1,9 @@
+
+story = "%s falliing from the sky, As you %s through the heavens. Taking a %s %s along the way, to get the object of truth. "
+
+noun = raw_input('add a singular or plural noun ')
+verb = raw_input('add a singular verb ')
+noun2 = raw_input('add a singular noun ')
+adjective = raw_input('add a singular adjective ')
+
+print story %(noun,verb,adjective,noun2)
