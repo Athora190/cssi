b2fd98b68446c3278e0f08952efa0938f1cebfce
Athora190 <jademarte7@gmail.com>
2018-08-07 09:36:21 -0400

    Python Quiz

---
+++ week3/pythonQuiz.py
@@ -0,0 +1,17 @@
+# task 1
+'''x = int(raw_input("Enter a number "))
+y = int(raw_input("Enter another number "))
+s = x + y
+d = x - y
+p = x * y
+if not y == 0:
+    q = x / y
+    r = x % y
+    print "the quotion is ", q , "\n The remainder is ", r
+print "The sum is ",s,"\n The difference is ",d,"\n The product is ",p
+'''
+#task 2
+num =range (1,101)
+for words in num:
+    fizz = int(num + 3)
+    print "Fizz"
