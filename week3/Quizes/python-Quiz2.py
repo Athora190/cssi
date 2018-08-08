9e20a3c09a37478d8c8cbdc3bc32ed0b116e1c4a
Athora190 <jademarte7@gmail.com>
2018-08-08 09:44:38 -0400

    quiz

--- week3/Quizes/python-Quiz2.py
+++ week3/Quizes/python-Quiz2.py
@@ -1,5 +1,18 @@
 # 1
 def smallestNumber(num1,num2,num3):
-    if num1 < :
-        print num
-smallestNumber()
+    if num1 < num2 and num1 < num3:
+        print num1
+    if num2 < num1 and num1 < num3:
+        print num2
+    if num3 < num1 and num3 < num2:
+        print num3
+smallestNumber(3,65,2)
+
+# 2 did not finish, looking at the board
+def sumOfEven(n):
+    sum = 0
+    for x in range(n):
+        sum = sum + 2 * x + 1
+    return sum
+print sum
+sumOfEven(3)
