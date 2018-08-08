02ac456cddba4f189e0e3add57e9bf2be3d251d2
Athora190 <jademarte7@gmail.com>
2018-08-08 14:10:38 -0400

    lab

--- week1/cssi-labs/python/labs/shopping-list-app/starter-code/shopping_list_starter.py
+++ week1/cssi-labs/python/labs/shopping-list-app/starter-code/shopping_list_starter.py
@@ -14,11 +14,18 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+
 choice = ""
 
 print("Welcome to the shopping list app!")
 
 shopping_list = []
+
+def printList():
+    print '###'
+    for i in shopping_list:
+        print i
+    print '\n'
 
 while choice.lower() != "e":
     print("Please choose your action from the following list:")
@@ -27,7 +34,42 @@ while choice.lower() != "e":
     print("c. Check to see if an item is on the list")
     print("d. Show all items on the list")
     print("e. exit")
-    
-    choice = input("Enter your choice [a|b|c|d|e]:")
-    
-    # Your code below! Handle the cases when the user chooses a, b, c, d, or e
+
+    choice = raw_input("Enter your choice [a|b|c|d|e]:")
+
+# Your code below! Handle the cases when the user chooses a, b, c, d, or e
+
+    if choice == 'a':
+        items = raw_input('Enter items ')
+        for i in items.split(','):
+            t = i.strip()
+            if not t in shopping_list:
+                shopping_list.append(t)
+            else:
+                print "you already have this item "
+
+
+    if 'b' == choice:
+        item = raw_input('Enter the name you want to remove ')
+        remove = raw_input("Are you sure you what to remove this item? ")
+        if remove == 'yes':
+            shopping_list.remove(item)
+        printList()
+
+    if 'c' == choice:
+        find = raw_input('What item are you looking for? ')
+        if find in shopping_list:
+            print find + ' is in your list'
+            item = raw_input('Would you like to remove it? ')
+            if item == 'yes':
+                shopping_list.remove(find)
+        else:
+            item = raw_input("would you like to add it? ")
+            if item == 'yes':
+                shopping_list.append(find)
+
+
+
+
+    if 'd' == choice:
+        print "The items in your list are " + str(printList())
